df9c9dcac832abca7818bd114a0ce446a30218b6
Phillip <phillip.besse@gmail.com>
2022-07-02 23:54:30 -0400

    search button saves value to local storage; adds line item to search history ul

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,25 @@
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
 
-
-
-
 var today = moment();
 $("#currentDay").text(today.format("MMMM Do, YYYY"));
+
+// Geocoding API
+// http:// api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+// Parameters
+// q	required	City name, state code (only for the US) and country code divided by comma. Please use ISO 3166 country codes.
+// appid	required	Your unique API key (you can always find it on your account page under the "API key" tab)
+// limit	optional	Number of the locations in the API response (up to 5 results can be returned in the API response)
+
+$("#search-button").on("click", function() {
+    var searchedCity = $(this).siblings("#search-city").val();
+    localStorage.setItem("city", searchedCity)
+    console.log(searchedCity);
+    // need to append searchedCity to ul to #search-city-list
+    $("#search-city-list").append("<li>" + searchedCity + "</li>");
+})
+
+
+function getLatLon() {
+    var requestURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + searchedCity + "&limit={limit}&appid={API key}"
+} 
